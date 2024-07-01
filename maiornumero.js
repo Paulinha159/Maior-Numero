ef1c8f1adec0cb5e2d2d2e8fbd55544c557151ee
Paula <paularebesco@gmail.com>
2024-07-01 10:10:38 -0300

    codigo

---
+++ maiornumero.js
@@ -0,0 +1,21 @@
+const prompt = require ('prompt-sync')();
+while(true){
+    let numero = prompt("Digite um numero: ")
+
+    if(isNaN(Number(numero))) {
+
+        console.log("Número inválido")
+    } else {
+     numero = numero.split("");
+    for (let i = 0; i < numero.length; i++){
+        for (let j = i+1 ; j < numero.length; j++) 
+            if(numero[i]< numero[j]){
+                let aux = numero[i]
+                numero[i]=numero[j]
+                numero[j]= aux
+
+            }
+            
+}console.log(numero.join(""))
+} 
+}
